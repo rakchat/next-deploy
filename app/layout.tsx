0d4128b8de7035a06a95cec77557b30ef696cbeb
suntorn.ra <suntorn.ra@logicspark.com>
2024-07-17 11:51:55 +0700

    💄 update style

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body className="container mx-auto">{children}</body>
     </html>
   );
 }
